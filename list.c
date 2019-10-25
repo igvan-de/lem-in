b89b2e69891dcca6aefd4d4aea6b5918551bf041
igvan-de <vdsmissenigor@gmail.com>
2019-10-25 12:23:48 +0200

    created add node function

--- list.c
+++ list.c
@@ -6,12 +6,17 @@
 /*   By: igvan-de <igvan-de@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/10/24 15:32:20 by igvan-de       #+#    #+#                */
-/*   Updated: 2019/10/24 16:15:31 by igvan-de      ########   odam.nl         */
+/*   Updated: 2019/10/24 16:53:58 by igvan-de      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lemin.h"
 
-void	add_node()
+void	add_node(t_link **node, t_link *new)
 {
+	if (*node == NULL || new == NULL)
+		return ;
+	new->next = *node;
+	*node = new;
 }
+
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: igvan-de <igvan-de@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/10/24 14:28:43 by igvan-de       #+#    #+#                */
-/*   Updated: 2019/10/24 16:33:05 by igvan-de      ########   odam.nl         */
+/*   Updated: 2019/10/24 16:48:26 by igvan-de      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,9 @@
 int		main(int argc, char **argv)
 {
 	char 	*line;
-	t_data	*node;
+	t_link	*node;
 
-	node = (t_data*)ft_memalloc(sizeof(t_data));
+	node = (t_link*)ft_memalloc(sizeof(t_link));
 	while (get_next_line(STDIN_FILENO ,&line) > 0)
 	{
 		//format_checker

c6839dfbf7abc3de6dbaa7448df91e804afd6ac3
igvan-de <vdsmissenigor@gmail.com>
2019-10-28 15:07:59 +0100

    changed parameters new_node function

--- list.c
+++ list.c
@@ -6,7 +6,7 @@
 /*   By: igvan-de <igvan-de@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/10/24 15:32:20 by igvan-de       #+#    #+#                */
-/*   Updated: 2019/10/25 15:47:58 by igvan-de      ########   odam.nl         */
+/*   Updated: 2019/10/28 15:07:30 by igvan-de      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,11 +31,14 @@ void	add_node(t_rooms **node, t_rooms *new)
 	prob->next = new;
 }
 
-t_rooms	*new_node(t_rooms **node)
+t_rooms	*new_node(char *str, int x, int y, int index)
 {
 	t_rooms	*new_node;
 
+	
 	new_node = (t_rooms*)ft_memalloc(sizeof(t_rooms));
+	// if (link == NULL)
+	// 	new_node->links = NULL;
 	if (new_node == NULL)
 		return (NULL);
 	new_node->next = NULL;

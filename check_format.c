b2d566f3349de21ffd4fb4f01eed3b7b67cb8f91
Yonne <yonnegroeneveld@gmail.com>
2019-11-19 16:29:46 +0100

    fix norm errors

--- check_format.c
+++ check_format.c
@@ -5,108 +5,105 @@
 /*                                                     +:+                    */
 /*   By: igvan-de <igvan-de@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2019/10/24 14:28:43 by igvan-de       #+#    #+#                */
-/*   Updated: 2019/11/19 16:08:58 by ygroenev      ########   odam.nl         */
+/*   Created: 2019/10/24 15:16:29 by igvan-de       #+#    #+#                */
+/*   Updated: 2019/11/19 16:29:34 by ygroenev      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lemin.h"
 
-static void		no_whitespaces(char *line) /*checking for whitespaces in front of line*/
+void			no_whitespaces(char *line) /*checking for whitespaces in front of line*/
 {
 	if (line[0] == ' ' || line[0] == '\t' || line[0] == '\n' ||
 		line[0] == '\v' || line[0] == '\f' || line[0] == '\r')
 	{
-		ft_putendl("error"); /*Error message to be determined*/
+		ft_putendl("Error! Whitespaces where there shouldn't be any"); /*Error message to be determined*/
 		exit(0);
 	}
 }
 
-static int	is_room(char *line, t_table **table, size_t size) //making sure links are excisting rooms
+int				check_if_command(char *line, t_ants **ants) /*ignoring all commands "#" and checking if they're start or end*/
 {
-	char	**a_b;
-	int		i;
-	t_table	*tmp;
+	int i;
 
-	a_b = ft_strsplit(line, '-');
 	i = 0;
-	while (i < size)
-	{
-		tmp = table[i];
-		while (tmp != NULL)
-		{
-			if (ft_strequ(tmp->name, a_b[A]) == TRUE ||
-				ft_strequ(tmp->name, a_b[B]) == TRUE)
-				return (TRUE);
-			tmp = tmp->next;
-		}
+	while (line && line[i])
 		i++;
-	}
-	return (FALSE);
-}
-
-int				check_if_command(char *line) /*ignoring all commands*/
-{
-	if (line[0] == '#')
+	if (i != 0 && line[0] == '#')
 	{
-		if (line[1] == '#')
-		{
-			if (line[2] != '#') /*make sure no segfault*/
-				return (TRUE);
-		}
+		if (i >= 1 && line[1] == '#')
+			is_start_or_end(line, ants);
+		return (TRUE);
 	}
 	return (FALSE);
 }
 
-int				check_format_room(char *line) /*making sure rooms are formatted correctly*/
+int				check_format_room(char *line, t_ants **ants) /*making sure rooms are formatted correctly*/
 {
-	/*make sure all rooms have a unique name and coordinates*/
 	int i;
 	int space_count;
 
 	i = 0;
 	space_count = 1; /*already counted first space because we skip it*/
 	no_whitespaces(line);
-	if (check_if_command(line) == TRUE)
+	if (check_if_command(line, ants) == TRUE)
 		return (TRUE);
+	if (!line[i])
+	{
+		ft_putendl("Error! Empty line"); /*Error message to be determined*/
+		exit(0);
+	}
 	while (line[i] && line[i] != ' ')
 		i++;
+	if (line[0] == 'L')
+	{
+		ft_putendl("Error! Room name can't start with 'L'"); /*Error message to be determined*/
+		exit(0);
+	}
 	i++;
 	while (line[i] && space_count < 3)
 	{
 		if (line[i] == ' ')
 			space_count++;
 		else if (ft_isdigit(line[i]) == FALSE)
-			return (FALSE);
+		{
+			ft_putendl("Error! Room coordinates must be numbers"); /*Error message to be determined*/
+			exit(0);
+		}
 		i++;
 	}
 	if (space_count == 2)
 		return (TRUE);
-	else
+	else /*No error if room is not formatted correctly. We don't want to exit because this function is supposed to return FALSE if it reaches the links*/
 		return (FALSE);
 }
 
-int				check_format_link(char *line, t_table **table, size_t size) //makng sure links are formatted correctly
-{ //skip commands.
+int				check_format_link(char *line, t_rooms **rooms) /*making sure links are formatted correctly*/
+{ //check for duplicate links (or do we not care there's duplicates?) //we don't care
 	int i;
 	int dash_count;
 
 	i = 0;
 	dash_count = 0;
 	no_whitespaces(line);
-	if (is_room(line, table, size) == FALSE)
-		return (FALSE);
+	if (line && line[0] == '#') /*skip commands*/
+		return (TRUE);
+	if (compare_with_rooms(lem_split(line, rooms), rooms) == FALSE)
+	{
+		ft_putendl("Error! One or more of the links points to an unexisting room"); /*Error message to be determined*/
+		exit(0);
+	}
 	while (line[i])
 	{
 		if (line[i] == '-')
 			dash_count++;
 		i++;
 	}
-	if (dash_count == 1)
+	if (dash_count >= 1)
 		return (TRUE);
 	else
 	{
-		ft_putendl("error"); //Error message to be determined
+		ft_putendl("Error! One or more of the links are not formatted correctly"); /*Error message to be determined*/
 		exit(0);
 	}
 }

6987af20d687ef3d883c9bc4932f33aebb2ab948
Yonne <yonnegroeneveld@gmail.com>
2019-12-17 15:57:15 +0100

    start of ants calc

--- ants_calc/ants_calc.c
+++ ants_calc/ants_calc.c
@@ -1,26 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   main.c                                             :+:    :+:            */
+/*   ants_calc.c                                        :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: igvan-de <igvan-de@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/10/24 14:28:43 by igvan-de       #+#    #+#                */
-/*   Updated: 2019/12/17 15:54:08 by ygroenev      ########   odam.nl         */
+/*   Updated: 2019/12/17 15:52:32 by ygroenev      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "lemin.h"
-
-int		main(void)
+void		ants_calc()
 {
-	t_rooms			*rooms;
-	t_ants			*ants;
+	/*
+	amount of ants
+	amount of paths
+	length of paths
 
-	ants = (t_ants*)ft_memalloc(sizeof(t_ants));
-	read_input(&rooms, &ants);
-	//free rooms?
-	//algorithm(table);
-	ants_calc();
-	return (FALSE);
-} //empty line at end of file doesn't give error, do we care?
+	calculate ants per path
+	*/
+}
